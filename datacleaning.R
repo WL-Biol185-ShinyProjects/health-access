13b9fd61d34a4ad148f13c1bf3f206e82d2d9b14
Caroline Cox <coxc22@mail.wlu.edu>
2021-10-18 16:12:40 -0400

    fix

--- datacleaning.R
+++ datacleaning.R
@@ -12,12 +12,7 @@ renamed<-
     pct_uninsured = `% Uninsured`, 
     num_primary_cp = `# Primary Care Physicians`, 
     rate_primary_cp = `Primary Care Physicians Rate`, 
-<<<<<<< HEAD
-    ratio_primary_cp = `Primary Care Physicians Ratio`
-  )
-glimpse(limited_data)
-view(limited_data)
-=======
+    ratio_primary_cp = `Primary Care Physicians Ratio`,
     ratio_primary_cp = `Primary Care Physicians Ratio`) 
 renamed$num_ratio_primary_cp<-as.numeric(substring(renamed$ratio_primary_cp, 1, nchar(renamed$ratio_primary_cp) -2))
 #state averages 
