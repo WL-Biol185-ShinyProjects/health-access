84ba24e694aa23e1dd405c810dc6711ed5414b70
luzaranm23 <luzaranm23@mail.wlu.edu>
2021-10-25 15:57:55 -0400

    maria changes to server.r

--- server.R
+++ server.R
@@ -5,30 +5,32 @@ library(htmltools)
 library(rgdal)
 library(tidyverse)
 
+
 function(input, output) {
  
 #Importing GeoSpatial Data
-  countiesGEO <- rgdal::readOGR("counties.json")
+  countiesGEO <- rgdal::readOGR("testing.json")
+
 
 #Trim data table for counties 
   Massachussetts <- 25
   
-  geo@data <- geo@data[which(geo@data$STATE == 25)]
-  geo@polygons[which(geo@data$STATE != 25)] <- NULL
+  countiesGEO@data <- countiesGEO@data[which(countiesGEO@data$STATE == 25),]
+  countiesGEO@polygons[which(countiesGEO@data$STATE != 25)] <- NULL
   
 #Output function for Massachussetts state & county map
   output$massachussetsMap <- renderLeaflet({
-    leaflet(counties.json) %>%
+    leaflet(countiesGEO) %>%
     addTiles() %>%
-    setView(-71.3824,42.4072, zoom = 10) %>% 
-    addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.5)
+    setView(-71.3824,42.4072, 5, zoom = 10) %>% 
+    addPolygons(color = "red", smoothFactor = 0.5, opacity = 0.5) 
+    
 })
 
       
   output$Nationmap <- renderLeaflet({
     leaflet(statesGEO) %>%
-    setView(-96, 37.8, 4) %>%
+    setView(-96, 37.8, 5) %>%
     addPolygons(weight = 2, opacity = 1, color = "white", 
                 dashArray = "3", fillOpacity = 0.7, 
                 highlightOptions = highlightOptions(
