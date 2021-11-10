2eb60c1b36608fe085be2959c0efe5cfc5201902
Tehila Adekogbe <adekogbet23@mail.wlu.edu>
2021-11-10 15:07:19 -0500

    changes

--- server.R
+++ server.R
@@ -16,23 +16,12 @@ function(input, output) {
   maGEO <- rgdal::readOGR("testing.json")
   alGEO <- rgdal::readOGR("testing.json")
   statesGEO <- rgdal::readOGR("states.geo.json")
-<<<<<<< HEAD
+
   bystateavgs <- read_csv("bystateavgs.csv")
-=======
-<<<<<<< HEAD
-  bystateavgs <- read_csv("bystateavgs.csv")
-=======
+
   stateavg_only <- read_csv("stateavg_only.csv")
->>>>>>> f2db7b51f075d86963e0e2656c2ed76e7bd97eb9
-  
-  
-  
-  #Trim data table for counties 
-  Massachussetts <- 25
-  
->>>>>>> 8ad7770efb5a79b92d1c3193e68d93c44d58fa1e
-  mass<- read_csv("massonly.csv")
-  
+
+
   
   #Note for later move above function and it will only be slow the first load not every load
   
@@ -43,6 +32,7 @@ function(input, output) {
   madata <- maGEO@data[which(maGEO@data$STATE == Massachussetts),]
   maGEO@polygons[which(maGEO@data$STATE != Massachussetts)] <- NULL
   maGEO@data <- madata
+  mass<- read_csv("massonly.csv")
 
   alabama <- "01"
   alabamadata <- alGEO@data[which(alGEO@data$STATE == alabama),]
@@ -93,34 +83,25 @@ function(input, output) {
   #Output for Nationmap      
   output$Nationmap <- renderLeaflet({
     #joining data 
-<<<<<<< HEAD
-    statesGEO@data<- left_join(statesGEO@data, bystateavgs, by= c("NAME" = "state"))
-    pal<- colorBin("Blues", domain = statesGEO@data$mean_pct_unins_by_state)
-=======
     statesGEO@data<- left_join(statesGEO@data, stateavg_only, by= c("NAME" = "state"))
     pal<- colorBin("Blues", domain = statesGEO@data[["input"]])
->>>>>>> f2db7b51f075d86963e0e2656c2ed76e7bd97eb9
     
     leaflet(statesGEO) %>%
       setView(-96, 37.8, 5) %>%
       addPolygons(weight = 2, opacity = 1, color = "white",
                   dashArray = "3", fillOpacity = 0.7, 
-                  fillColor = ~pal(mean_pct_unins_by_state),
+                  fillColor = ~pal(pct_uninsured),
                   highlightOptions = highlightOptions(
                     weight = 5,
                     color = "#666",
                     dashArray = "",
                     fillOpacity = 0.7,
                     bringToFront = TRUE),
-                  label = ~paste0(NAME, ": ", formatC(statesGEO@data$mean_pct_unins_by_state))
+                  label = ~paste0(NAME, ": ", formatC(statesGEO@data$pct_uninsured))
       ) %>%
       addLegend("bottomright",
                 pal = pal,
-<<<<<<< HEAD
-                values = ~(statesGEO@data$mean_pct_unins_by_state),
-=======
-                values = ~(statesGEO@data[["input"]]),
->>>>>>> f2db7b51f075d86963e0e2656c2ed76e7bd97eb9
+                values = ~(statesGEO@data$pct_uninsured),
                 opacity = 0.8,
                 title = "Mean Percent Uninsured by State",
                 labFormat = labelFormat(suffix = "%")
