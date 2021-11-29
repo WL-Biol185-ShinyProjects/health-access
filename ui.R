28625fff7294682e148141455fc9b700f72ccb95
Tehila Adekogbe <adekogbet23@mail.wlu.edu>
2021-11-28 19:46:10 -0500

    Changes to Welcome page and addition of plots

--- ui.R
+++ ui.R
@@ -24,34 +24,35 @@ dashboardPage(
     tabItems(
       # First tab content
       tabItem(tabName = "Welcome", 
+              titlePanel(tags$h1(tags$div(tags$b(tags$u("Access to Healthcare in the United States"))), align = "center")),
               fluidRow(
-                       tags$h2(tags$div(tags$b(tags$u("Goals"))), align = "center"), 
-                       br(), 
-                       tags$div(img(src = "https://epsa-online.org/LLeaP/wp-content/uploads/2019/04/access_banner-1920x1008.png",
-                                    height = 200, width = 400), align = "center"),
-                       br(),
-                       tags$div(p("Health care accessibility is a complex topic that is influenced by various socio-economic 
-                       factors.The goal of this app is to provide an overview of the percentage of insured adults that
-                       reside in each state and the number of primary physicians available in each state. A state with
-                       relatively high and relatively low healthcare access based on these two factors were also identified. 
-                        We analyzed these two states on the county level in an effort to identify trends that may cause 
-                        the accessibility gap between these two locations.")), 
-                       br(), 
-                       br(), 
-                       tags$div(img(src = "https://medcitynews.com/wp-content/uploads/2017/02/GettyImages-109420355-1.jpg", 
-                                    height = 200, width = 400), align = "center"), 
-                       br(), 
-                       tags$div(p("Identifying states and counties with low and healthcare accessibility is the first step 
-                         in creating a long-term plan to address this issue. Future analysis can focus on the 
-                         causative factors behind these trends. Studying policies and environmental factors within
-                         high healthcare access areas could also help form strategies that can be implemented in low
-                         access areas to improve quality of care.")),
-                       tags$u(div(h3(strong("Want to Learn More")))), 
-                       br(),
-                       tags$a(href="https://www.countyhealthrankings.org/explore-health-rankings/rankings-data-documentation", "Source of Nationwide and Statewide Health Data"), 
-                       br(), 
-                       tags$a(href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7999346/", "Acccess to Healthcare during COVID-19") 
-                       
+                column(width = 5, box(align = "center", width = 400, solidHeader = TRUE, status = "primary",
+                       img(src = "https://epsa-online.org/LLeaP/wp-content/uploads/2019/04/access_banner-1920x1008.png", 
+                           height = "225", width = "255"),
+                       img(src = "https://medcitynews.com/wp-content/uploads/2017/02/GettyImages-109420355-1.jpg", 
+                           height = "225", width = "255"))),
+                column(width = 7, 
+                       box(tags$h2(tags$div(tags$b(tags$u("Goals"))), align = "center"), 
+                           width = 12, solidHeader = TRUE, status = "primary",
+                           tags$li("Health care accessibility is a complex topic that is influenced by various socio-economic 
+                           factors.The goal of this app is to provide an overview of the percentage of insured adults that
+                           reside in each state and the number of primary physicians available in each state. A state with
+                           relatively high and relatively low healthcare access based on these two factors were also identified. 
+                           We analyzed these two states on the county level in an effort to identify trends that may cause
+                           the accessibility gap between these two locations."), 
+                           br(),
+                           tags$li("Identifying states and counties with low and healthcare accessibility is the first step 
+                           in creating a long-term plan to address this issue. Future analysis can focus on the 
+                           causative factors behind these trends. Studying policies and environmental factors within
+                           high healthcare access areas could also help form strategies that can be implemented in low
+                           access areas to improve quality of care."))),
+                box(tags$u(h3(strong("Want to Learn More"))), width = 5, solidHeader = TRUE, status = "primary",
+                br(),
+                tags$li(tags$a(href="https://www.countyhealthrankings.org/explore-health-rankings/rankings-data-documentation",
+                       "Source of Nationwide and Statewide Health Data")), 
+                br(), 
+                tags$li(tags$a(href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7999346/",
+                       "Acccess to Healthcare during COVID-19")))
                 )),
       
       # Second tab content
@@ -77,7 +78,8 @@ dashboardPage(
               ),
             mainPanel(
               leafletOutput("massachussetsMap"),
-              plotOutput("MASShist")
+              plotOutput("MASSpoint"),
+              plotOutput(ALvsMASS)
               )
             )
           )
@@ -115,7 +117,8 @@ dashboardPage(
             ),
           mainPanel(
             leafletOutput("alabamaMap"),
-            plotOutput("ALhist")
+            plotOutput("ALpoint"),
+            plotOutput(ALvsMASS)
             )
           )
         )
@@ -151,7 +154,7 @@ dashboardPage(
             ),
            mainPanel(
              leafletOutput("Nationmap"),
-             plotOutput("STATEhist")
+             plotOutput("STATEbar")
            )
           )
         )
