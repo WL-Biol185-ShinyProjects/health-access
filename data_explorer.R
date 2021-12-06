fdcce1578db653f34f3c7f2586184bd5d6c113e7
luzaranm23 <luzaranm23@mail.wlu.edu>
2021-12-06 14:03:09 -0500

    Added data explorer tab & drafts for scatterplot

--- data_explorer.R
+++ data_explorer.R
@@ -1,4 +1,4 @@
-output$dataexplorer <- renderDataTable({
+output$HAdataexplorer <- renderDataTable({
   tidystateavg_only %>%
     select(FIPS, state, county, preventable_hr, pct_uninsured, num_primary_cp, 
            rate_primary_cp, ratio_primary_cp, adult_obesity, pct_highschool_completed, 
@@ -6,3 +6,65 @@ output$dataexplorer <- renderDataTable({
            '% Asian', '% Native Hawaiian /Other Pacific Islander', '% Hispanic', 
            '% Non-Hispanic White', '% Not Proficient in English', Population.1)
 })
+
+#Working Data Table - Sixth tab contents - UI
+tabItem(
+  tabName ="DataExplorer",
+  fluidRow(
+    column(12, dataTableOutput("HAdataexplorer"))
+    
+#Working Data Table - Side tabs - UI
+menuItem("DataExplorer", tabName = "DataExplorer")
+
+#Working Data Table - Server
+output$HAdataexplorer <- renderDataTable (stateavg_only, 
+                                          escape = 1:21)
+
+# Error for data table 
+DataTables warning: table id=DataTables_Table_0 - Requested unknown parameter '5' for row 0. For more information about this error, please see http://datatables.net/tn/4
+
+
+
+
+output$HAdataexplorer <- renderDataTable ({
+  stateavg_only})
+
+fluidPage(
+  fluidRow(
+    mainPanel(
+      tableOutput("HAdataexplorer")
+      
+# Scatter Plot relating --> num_ratio_primary_cp & % Non-Hispanic White
+# Scatter Plot relationg --> pct_uninsured & pct_highschool_completed
+ggplot(mass, aes(x = "% Non-Hispanic White", y = num_ratio_primary_cp)) 
++ geom_point(size=2, shape = 23)
++ geom_text(label = rownames(county))
++ geom_smooth(method = lm, se = FALSE),
+
+ggplot(mass, aes(x = pct_highschool_completed, y = pct_uninsured)) 
++ geom_point(size = 2, shape = 23 )
++ geom_text(label = rownames(county))
++ geom_smooth(method = lm, se = FALSE)
+
+
+#What I deleted from server
+
+output$HAdataexplorer <- renderDataTable (stateavg_only, 
+                                          escape = 1:21)
+print(stateavg_only)
+
+output$trial <- renderPlot({
+  ggplot(mass, aes(x = pct_highschool_completed, y = pct_uninsured)) +
+    geom_point(size = 2, shape = 23 ) + 
+    geom_smooth(method = lm, se = FALSE)
+})
+
+output$trial2 <- renderPlot({ 
+  ggplot(mass, aes(x = "% Non-Hispanic White", y = num_ratio_primary_cp)) + 
+    geom_point(size=2, shape = 23) + 
+    geom_smooth(method = lm, se = FALSE)
+  
+})
+
+
+} 
